92c79513d8503f6e0c6dc54c6404a861f080946f
ayan-kundu <75118735+ayan-kundu@users.noreply.github.com>
2021-01-17 03:17:04 -0500

    Update obstacle_avoid.py

--- obstacle_avoid.py
+++ obstacle_avoid.py
@@ -1,16 +1,16 @@
 #alike simple wall following alorithm
 
 heading=0
-if min dist_range<2:
+if min_dist <2:
 	turn left 45
 	heading+=45
-	if min dist_range<2:
+	if min_dist <2:
 		turn right 90
 		heading-=45
-		if min dist_range<2:
+		if min_dist <2:
 			turn right 45
 			heading-=90
-			if min dist_range<2:
+			if min_dist <2:
 				turn left 180
 				heading+=90
 			else:
